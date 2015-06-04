b8950ce8de0e653730780866da6bb03d9b3a0f4e
daward <acheron0@gmail.com>
2015-06-04 00:09:21 -0400

    Little more entry point cleanup

--- dominioneer.js
+++ dominioneer.js
@@ -1,5 +1,9 @@
 var GameBuilder = require('./gamebuilder.js');
 var History = require('./history.js');
+var Deck = require('./deck.js');
+var Game = require('./game.js');
 
 module.exports.GameBuilder = GameBuilder;
 module.exports.History = History;
+module.exports.deck = new Deck();
+module.exports.Game = Game;
